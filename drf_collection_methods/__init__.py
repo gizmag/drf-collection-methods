2a73aa261201b8f498b1ced06ac5423ac2d6a9a6
Jacob Haslehurst <jacob@haslehurst.net>
2014-08-07 18:55:05 +1000

    do the thing in get_routes

--- drf_collection_methods/__init__.py
+++ drf_collection_methods/__init__.py
@@ -1,7 +1,39 @@
-class CollectionMethodRouterMixin(object):
-    def get_routes(self, viewset):
-        # do some magic to hooked up our decorated methods
+from rest_framework.routers import replace_methodname, Route
 
+
+class CollectionMethodRouterMixin(object):
+    collection_route = Route(
+        url=r'^{prefix}/{methodname}{trailing_slash}$',
+        mapping={
+            '{httpmethod}': '{methodname}',
+        },
+        name='{basename}-{methodnamehyphen}',
+        initkwargs={}
+    )
+
+    def get_routes(self, viewset):
+        ret = super(CollectionMethodRouterMixin, self).get_routes(viewset)
+
+        dynamic_routes = []
+        for methodname in dir(viewset):
+            attr = getattr(viewset, methodname)
+            httpmethods = getattr(attr, 'bind_to_collection_methods', None)
+            if httpmethods:
+                httpmethods = [method.lower() for method in httpmethods]
+                dynamic_routes.append((httpmethods, methodname))
+
+        route = self.collection_route
+        for httpmethods, methodname in dynamic_routes:
+            initkwargs = route.initkwargs.copy()
+            initkwargs.update(getattr(viewset, methodname).kwargs)
+            ret.append(Route(
+                url=replace_methodname(route.url, methodname),
+                mapping=dict((httpmethod, methodname) for httpmethod in httpmethods),
+                name=replace_methodname(route.name, methodname),
+                initkwargs=initkwargs,
+            ))
+
+        return ret
 
 def collection_link(**kwargs):
     def decorator(func):
